2ced20a86cb76ec5ced3580c2fe48f02f6263b9b
ichiche <chicheng@microsoft.com>
2021-10-01 18:12:37 +0800

    Add Region Section
    Improve Outlining

--- Create-Image-FromDisk.ps1
+++ Create-Image-FromDisk.ps1
@@ -1,19 +1,23 @@
-# Global Parameter
+#Region Global Parameter
 $NewImageName = ""
 $osDiskName = "" # Enter OS Disk Name of VM
 $rgName = "" # Enter Resource Group Name
 $location = "" # Enter Azure Resource Location, e.g. "East Asia"
 $subscriptionId = "" # Enter Subscription ID
+#EndRegion Global Parameter
 
-# Login
+#Region Login
 Connect-AzAccount # Comment this line if using Connect-To-Cloud.ps1
+#EndRegion Login
 
-# Main
+#Region Main
 Set-AzContext -SubscriptionId $subscriptionId
 $managedDiskID = "/subscriptions/$subscriptionId/resourceGroups/$rgName/providers/Microsoft.Compute/disks/$osDiskName"
 $imageConfig = New-AzImageConfig -Location $location # -HyperVGeneration V2
 $imageConfig = Set-AzImageOsDisk -Image $imageConfig -OsState Generalized -OsType Windows -ManagedDiskId $managedDiskID
 New-AzImage -ImageName $NewImageName -ResourceGroupName $rgName -Image $imageConfig
+#EndRegion Main
 
-# Logout
+#Region Logout
 Disconnect-AzAccount
+#EndRegion Logout
