636badf105fd397870e6c391e01edefa950adb37
ichiche <chicheng@microsoft.com>
2021-10-02 02:40:15 +0800

    Delete Encrypt-Password-ReadHost.ps1

--- Encrypt-Password-ReadHost.ps1
+++
@@ -1,4 +0,0 @@
-# Main
-$password = Read-Host -Prompt "Enter your Password of Azure Account, then press ENTER" -AsSecureString
-$bytes = ConvertFrom-SecureString $password
-$bytes | Out-File .\secure-password.txt
