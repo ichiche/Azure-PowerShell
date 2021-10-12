7b8295448cab138c46103ec21ceb5c36d4498527
ichiche <chicheng@microsoft.com>
2021-10-12 22:04:02 +0800

    Update Set-Static-PrivateIp.ps1

--- Virtual Machine/Set-Static-PrivateIp.ps1
+++ Virtual Machine/Set-Static-PrivateIp.ps1
@@ -1,34 +1,12 @@
-
-# Azure RM Module
+# Script Variable
 $info = @()
-$vms = Get-AzureRmVM | sort Name
-$nics = Get-AzureRmNetworkInterface | ? {$_.VirtualMachine -ne $null} | sort Name  #skip Nics with no VM
 
-foreach($nic in $nics)
-{
-    $vm = $vms | ? {$_.Id -eq $nic.VirtualMachine.id}
-    $prv =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAddress
-    $alloc =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAllocationMethod
-
-    $obj = New-Object -TypeName PSobject
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "Name" -Value $($vm.Name)
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "IpAddress" -Value $prv
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "Network Interface" -Value $nic.Name
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "Type" -Value $alloc
-    $info += $obj
-}
-
-$info = $info | sort Name
-$info | Export-Csv C:\Temp\ARMVM.csv -NoTypeInformation -Force -Confirm:$false
-
-# Az Module
-$vms = Get-AzVM -Status | sort Name # Contain Network Interface Info
-$vms = $vms | ? {$classicVMInfo.Name -contains $_.Name} # Gather information of imported list for Classic VM
+# Collect VM and NIC Information
+$vms = Get-AzVM -Status
 $nics = Get-AzNetworkInterface  | ? {$_.VirtualMachine -ne $null} | sort Name #skip Nics with no VM
-$info = @()
 
-foreach($nic in $nics)
-{
+# Map VM to NIC
+foreach($nic in $nics) {
     $vm = $vms | ? {$_.Id -eq $nic.VirtualMachine.id}
     $prv =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAddress
     $alloc =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAllocationMethod
@@ -36,39 +14,38 @@ foreach($nic in $nics)
     $obj = New-Object -TypeName PSobject
     Add-Member -InputObject $obj -MemberType NoteProperty -Name "Name" -Value $($vm.Name)
     Add-Member -InputObject $obj -MemberType NoteProperty -Name "IpAddress" -Value $prv
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "Network Interface" -Value $nic.Name
-    Add-Member -InputObject $obj -MemberType NoteProperty -Name "Type" -Value $alloc
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "NetworkInterface" -Value $nic.Name
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name "AllocationMethod" -Value $alloc
     $info += $obj
 }
 
-$info = $info | sort Name
-
-# Compare
-foreach ($item in $classicVMInfo) {
-    $currentVM = $info | ? {$_.Name -eq $item.Name}
-
-    if ($currentVM -ne $null) {
-        [string]$currentVMIpAddress = $currentVM.IpAddress
-
-        if ($item.IpAddress -ne $currentVMIpAddress) {
-            Write-Host ($item.Name + " IP address not match")
-        }
-    } else {
-        Write-Host ($item.Name + " not found")
-    }
-}
-
+# Display result to console 
+$info | ft Name, IpAddress, NetworkInterface, AllocationMethod -AutoSize
 
 # Set PrivateIpAllocationMethod to Static
-foreach($nic in $nics)
-{
+Write-Host ("`nProcessing to set Private Ip Allocation to Static`n") -ForegroundColor Yellow
+foreach($nic in $nics) {
     $vm = $vms | ? {$_.Id -eq $nic.VirtualMachine.id}
-    $alloc =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAllocationMethod
+    $NicIpConfigurations =  $nic.IpConfigurations | select -ExpandProperty PrivateIpAllocationMethod
 
-    if ($alloc -ne "Static") {
-        $Nic = Get-AzNetworkInterface -ResourceGroupName $nic.ResourceGroupName -Name $nic.Name
-        $Nic.IpConfigurations[0].PrivateIpAllocationMethod = "Static"
-        Set-AzNetworkInterface -NetworkInterface $Nic
+    for ($i = 0; $i -lt $NicIpConfigurations.Count; $i++) {
+        # Multiple IP Address assigned to single NIC
+        if ($NicIpConfigurations.Count -gt 1) {
+            #Write-Host ($vm.Name + " - " + $nic.Name + " consist of multiple IP Address, IP" + $i + " currently " + $NicIpConfigurations[$i] + " Allocation") 
+            $alloc = $NicIpConfigurations[$i]
+        } else {
+            #Write-Host ($vm.Name + " - " + $nic.Name)
+            $alloc = $NicIpConfigurations
+        }
+
+        if ($alloc -ne "Static") {
+            Write-Host ($vm.Name + " - " + $nic.Name + " changed from Dynamic to Static")
+            $item = Get-AzNetworkInterface -ResourceGroupName $nic.ResourceGroupName -Name $nic.Name
+            $item.IpConfigurations[$i].PrivateIpAllocationMethod = "Static"
+            Set-AzNetworkInterface -NetworkInterface $item
+        }
     }
 }
 
+# End
+Write-Host ("`nCompleted`n") -ForegroundColor Yellow
