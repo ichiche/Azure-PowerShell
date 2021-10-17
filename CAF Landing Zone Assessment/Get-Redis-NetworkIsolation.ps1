0fe7f665097615a56178576affd128e9fdfd4399
ichiche <chicheng@microsoft.com>
2021-10-17 12:52:01 +0800

    Update Get-Redis-NetworkIsolation.ps1

--- CAF Landing Zone Assessment/Get-Redis-NetworkIsolation.ps1
+++ CAF Landing Zone Assessment/Get-Redis-NetworkIsolation.ps1
@@ -1,16 +1,81 @@
-# Get Redis Cache Network Status
+# Global Parameter
+$CsvFullPath = "C:\Temp\Azure-Redis-Configuration.csv" # Export Result to CSV file 
 
+# Script Variable
+$Global:RedisCacheSetting = @()
+[int]$CurrentItem = 1
+$ErrorActionPreference = "SilentlyContinue"
 
+# Login
 az login
-az account set --subscription "subscription_id"
-$redis = az redis show --resource-group "rg_name" --name "instance_name"
-$redis = $redis | ConvertFrom-Json
 
-# 1 Redis VNet integration 
-# Confirm the status by below properties 
-$redis.sku.Name -eq "Premium"
-$redis.subnetId -eq $null
+# Get Azure Subscription
+$Subscriptions = az account list | ConvertFrom-Json
 
-# 2 Redis Private Endpoint
-$redis.privateEndpointConnections -eq $null # Check if no endpoint is added
-$redis.publicNetworkAccess -eq "Enabled" # Check if allow public access is enabled
+# Main
+foreach ($Subscription in $Subscriptions) {
+    az account set --subscription $Subscription.Id
+    Write-Host ("`nProcessing " + $CurrentItem + " out of " + $Subscriptions.Count + " Subscription: " + $Subscription.name + "`n") -ForegroundColor Yellow
+    $CurrentItem++
+    
+    # Get the Redis Cache List of current subscription
+    $RedisCaches = az redis list | ConvertFrom-Json
+
+    # Get configuration of each Redis Cache
+    foreach ($RedisCache in $RedisCaches) {
+        # SKU
+        $sku = ($RedisCache.sku.name + ": " + $RedisCache.sku.family + $RedisCache.sku.capacity)
+
+        # Zone
+        if ($RedisCache.zones -eq $null) {
+            $EnabledZoneRedundant = "N"
+            $DeployedZoneRedundant = "N/A"
+        } else {
+            $EnabledZoneRedundant = "Y"
+            $DeployedZoneRedundant = $RedisCache.zones
+        }
+
+        # Redis vNet integration 
+        if ($RedisCache.sku.name -eq "Premium" -and $RedisCache.subnetId -ne $null) {
+            $EnabledVNetIntegration = "Y"
+        } else {
+            $EnabledVNetIntegration = "N"
+        }
+
+        # Redis Private Endpoint
+        if ($RedisCache.privateEndpointConnections -ne $null) {
+            $EnabledPrivateEndpoint = "Y"
+        } else {
+            $EnabledPrivateEndpoint = "N"
+        }
+
+        if ($RedisCache.publicNetworkAccess -ne "Enabled") {
+            $AllowPrivateEndpointOnly = "Y"
+        } else {
+            $AllowPrivateEndpointOnly = "N"
+        }
+
+         # Save to Temp Object
+        $obj = New-Object -TypeName PSobject
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "SubscriptionName" -Value $Subscription.name
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "SubscriptionId" -Value $Subscription.id
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "ResourceGroup" -Value $RedisCache.resourceGroup
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "ResourceName" -Value $RedisCache.name
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "Size" -Value $sku
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "Location" -Value $RedisCache.location
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "EnabledZoneRedundant" -Value $EnabledZoneRedundant
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "DeployedZoneRedundant" -Value $DeployedZoneRedundant
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "EnabledVNetIntegration" -Value $EnabledVNetIntegration
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "EnabledPrivateEndpoint" -Value $EnabledPrivateEndpoint
+        Add-Member -InputObject $obj -MemberType NoteProperty -Name "AllowPrivateEndpointOnly" -Value $AllowPrivateEndpointOnly
+
+        # Save to Array
+        $Global:RedisCacheSetting += $obj
+    }
+}
+
+# Export Result to CSV file 
+$Global:RedisCacheSetting | sort InstanceType, SubscriptionName | Export-Csv -Path $CsvFullPath -NoTypeInformation -Confirm:$false -Force
+
+# End
+Write-Host "`nCompleted`n" -ForegroundColor Yellow
