6a53097132fcb569607ec7cf625f2ee8c577f520
ichiche <chicheng@microsoft.com>
2022-06-08 23:54:52 +0800

    Create Transform-Policy.ps1

---
+++ Security/Transform-Policy.ps1
@@ -0,0 +1,123 @@
+<#
+    .SYNOPSIS
+        Given an Azure firewall policy id the script will transform it to a Premium Azure firewall policy. 
+        The script will first pull the policy, transform/add various parameters and then upload a new premium policy. 
+        The created policy will be named <previous_policy_name>_premium if no new name provided else new policy will be named as the parameter passed.  
+    .Example
+        Transform-Policy -PolicyId /subscriptions/XXXXX-XXXXXX-XXXXX/resourceGroups/some-resource-group/providers/Microsoft.Network/firewallPolicies/policy-name -NewPolicyName <optional param for the new policy name>
+#>
+
+param (
+    #Resource id of the azure firewall policy. 
+    [Parameter(Mandatory=$true)]
+    [string]
+    $PolicyId,
+
+    #new filewallpolicy name, if not specified will be the previous name with the '_premium' suffix
+    [Parameter(Mandatory=$false)]
+    [string]
+    $NewPolicyName = ""
+)
+$ErrorActionPreference = "Stop"
+$script:PolicyId = $PolicyId
+$script:PolicyName = $NewPolicyName
+
+function ValidatePolicy {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [Object]
+        $Policy
+    )
+
+    Write-Host "Validating resource is as expected"
+
+    if ($null -eq $Policy) {
+        Write-Error "Recived null policy"
+        exit(1)
+    }
+    if ($Policy.GetType().Name -ne "PSAzureFirewallPolicy") {
+        Write-Host "Resource must be of type Microsoft.Network/firewallPolicies" -ForegroundColor Red
+        exit(1)
+    }
+
+    if ($Policy.Sku.Tier -eq "Premium") {
+        Write-Host "Policy is already premium"
+        exit(1)
+    }
+}
+
+function GetPolicyNewName {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [Microsoft.Azure.Commands.Network.Models.PSAzureFirewallPolicy]
+        $Policy
+    )
+
+    if (-not [string]::IsNullOrEmpty($script:PolicyName)) {
+        return $script:PolicyName
+    }
+
+    return $Policy.Name + "_premium"
+}
+
+function TransformPolicyToPremium {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [Microsoft.Azure.Commands.Network.Models.PSAzureFirewallPolicy]
+        $Policy
+    )    
+    $NewPolicyParameters = @{
+                        Name = (GetPolicyNewName -Policy $Policy) 
+                        ResourceGroupName = $Policy.ResourceGroupName 
+                        Location = $Policy.Location 
+                        ThreatIntelMode = $Policy.ThreatIntelMode 
+                        BasePolicy = $Policy.BasePolicy.Id 
+                        DnsSetting = $Policy.DnsSettings 
+                        Tag = $Policy.Tag 
+                        SkuTier = "Premium" 
+    }
+
+    Write-Host "Creating new policy"
+    $premiumPolicy = New-AzFirewallPolicy @NewPolicyParameters
+
+    Write-Host "Populating rules in new policy"
+    foreach ($ruleCollectionGroup in $Policy.RuleCollectionGroups) {
+        $ruleResource = Get-AzResource -ResourceId $ruleCollectionGroup.Id
+        $ruleToTransfom = Get-AzFirewallPolicyRuleCollectionGroup -AzureFirewallPolicy $Policy -Name $ruleResource.Name
+        $ruleCollectionGroup = @{
+            FirewallPolicyObject = $premiumPolicy
+            Priority = $ruleToTransfom.Properties.Priority
+            Name = $ruleToTransfom.Name
+        }
+
+        if ($ruleToTransfom.Properties.RuleCollection.Count) {
+            $ruleCollectionGroup["RuleCollection"] = $ruleToTransfom.Properties.RuleCollection
+        }
+
+        Set-AzFirewallPolicyRuleCollectionGroup @ruleCollectionGroup
+    }
+}
+
+function ValidateAzNetworkModuleExists {
+    Write-Host "Validating needed module exists"
+    $networkModule = Get-InstalledModule -Name "Az.Network" -MinimumVersion 4.5 -ErrorAction SilentlyContinue
+    if ($null -eq $networkModule) {
+        Write-Host "Please install Az.Network module version 4.5.0 or higher, see instructions: https://github.com/Azure/azure-powershell#installation"
+        exit(1)
+    }
+    $resourceModule = Get-InstalledModule -Name "Az.Resources" -MinimumVersion 4.2 -ErrorAction SilentlyContinue
+    if ($null -eq $resourceModule) {
+        Write-Host "Please install Az.Resources module version 4.2.0 or higher, see instructions: https://github.com/Azure/azure-powershell#installation"
+        exit(1)
+    }
+    Import-Module Az.Network -MinimumVersion 4.5.0
+    Import-Module Az.Resources -MinimumVersion 4.2.0
+}
+
+ValidateAzNetworkModuleExists
+$policy = Get-AzFirewallPolicy -ResourceId $script:PolicyId
+ValidatePolicy -Policy $policy
+TransformPolicyToPremium -Policy $policy
