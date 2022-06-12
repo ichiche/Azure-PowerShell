6f12a0dbd2e5a8e0705d102aa26ca1b6b4a1b324
ichiche <chicheng@microsoft.com>
2022-06-12 14:17:59 +0800

    Update Start-Stop-VmssInstance.ps1

--- Virtual Machine/Start-Stop-VmssInstance.ps1
+++ Virtual Machine/Start-Stop-VmssInstance.ps1
@@ -1,20 +1,23 @@
-# Stop VM in Scale Set that runnign in specific Availability Zone
-$vmss = Get-AzVmssVM -ResourceGroupName MC_AKS_aks-core-prd-001_eastus -VMScaleSetName aks-apppool-19256404-vmss -InstanceView
-$vmss | select Name, InstanceID, Zones, @{n="PowerState";e={$($_.InstanceView.Statuses.DisplayStatus | ? {$_ -notlike "Provisioning*"})}}
-$list = $vmss | ? {$_.Zones -eq 1}
-$InstanceIds = @()
-foreach ($item in $list) { $InstanceIds += "$($item.InstanceID)"} 
-$InstanceIds
+# Global Parameter
+$VmssRG = "MC_AKS_aks-core-prd-001_eastus"
+$VmssName = "aks-apppool-19256404-vmss"
+$SelectedAZone = 1
 
-Stop-AzVmss -InstanceId $InstanceIds -ResourceGroupName MC_AKS_aks-core-prd-001_eastus -VMScaleSetName aks-apppool-19256404-vmss -Confirm:$false -Force
+# Script Variable
+
+# Retrieve the VM Instance in VM Scale Set with Zone and Power State
+$vmss = Get-AzVmssVM -ResourceGroupName $VmssRG -VMScaleSetName $VmssName -InstanceView
+$vmss | ft Name, InstanceID, Location, Zones, @{n="PowerState";e={$($_.InstanceView.Statuses.DisplayStatus | ? {$_ -notlike "Provisioning*"})}} -AutoSize
+
+# Stop VM in Scale Set that provisioned in specific Availability Zone
+$InstanceIds = @()
+$list = $vmss | ? {$_.Zones -eq $SelectedAZone}
+foreach ($item in $list) { $InstanceIds += "$($item.InstanceID)"} 
+Stop-AzVmss -InstanceId $InstanceIds -ResourceGroupName $VmssRG -VMScaleSetName $VmssName -Confirm:$false -Force
 
 # Start VM in Scale Set that is not in running state
-
-$vmss = Get-AzVmssVM -ResourceGroupName MC_AKS_aks-core-prd-001_eastus -VMScaleSetName aks-apppool-19256404-vmss -InstanceView
+$vmss = Get-AzVmssVM -ResourceGroupName $VmssRG -VMScaleSetName $VmssName -InstanceView
 $list = $vmss | ? {$_.InstanceView.Statuses.DisplayStatus -notcontains "VM Running"}
 $InstanceIds = @()
 foreach ($item in $list) { $InstanceIds += "$($item.InstanceID)"} 
-$InstanceIds
-
-# Start-AzVmss: A parameter cannot be found that matches parameter name 'Force'.
-Start-AzVmss -InstanceId $InstanceIds -ResourceGroupName MC_AKS_aks-core-prd-001_eastus -VMScaleSetName aks-apppool-19256404-vmss -Confirm:$false
+Start-AzVmss -InstanceId $InstanceIds -ResourceGroupName $VmssRG -VMScaleSetName $VmssName -Confirm:$false
