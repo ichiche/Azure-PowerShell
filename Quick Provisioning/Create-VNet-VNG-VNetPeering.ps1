ec5feee86e4fddeb56804cf96c189b25cc62f71f
ichiche <chicheng@microsoft.com>
2022-07-27 12:05:40 +0800

    Update Create-VNet-VNG-VNetPeering.ps1

--- Quick Provisioning/Create-VNet-VNG-VNetPeering.ps1
+++ Quick Provisioning/Create-VNet-VNG-VNetPeering.ps1
@@ -1,29 +1,35 @@
 # Global Parameter
 $Location = "East Asia"
-$HubRG = "rg-gpsn01-pd-eas-hub01"
-$DmzRG = "rg-gpsn01-pd-eas-dmz01"
+$HubRG = "rg-hub-eas-hub01"
+$HubVNetName = "vnet-hub-quickstart-eas-001"
+$SpokeRG = "rg-spoke-pd-eas-app01"
+$SpokeVNetName = "vnet-spoke-quickstart-eas-001"
+$VngName = "vng-quickstart-prd-eas-001"
+$GatewayType = "Vpn" # Vpn or ExpressRoute
+$pipName = "pip-vng-quickstart-prd-eas-001"
 
 # Main
 $StartTime = Get-Date
 
 #Region Resource Group
 Write-Host "`nProvision Resource Group ..." -ForegroundColor Cyan
+Write-Host ("`n[LOG] " + (Get-Date -Format "yyyy-MM-dd hh:mm")) -ForegroundColor White -BackgroundColor Black
 
 # Create Resource Group if not exist
 $IsExist = Get-AzResourceGroup -Name $HubRG -ErrorAction SilentlyContinue
 if ([string]::IsNullOrEmpty($IsExist)) {
     New-AzResourceGroup -Name $HubRG -Location $Location | Out-Null
-    Write-Host ($HubRG + " is created") 
+    Write-Host ("Resource Group " + $HubRG + " is created") 
 } else {
-    Write-Host ($HubRG + " already exist") -ForegroundColor Yellow
+    Write-Host ("Resource Group " + $HubRG + " already exist") -ForegroundColor Yellow
 }
 
-$IsExist = Get-AzResourceGroup -Name $DmzRG -ErrorAction SilentlyContinue
+$IsExist = Get-AzResourceGroup -Name $SpokeRG -ErrorAction SilentlyContinue
 if ([string]::IsNullOrEmpty($IsExist)) {
-    New-AzResourceGroup -Name $DmzRG -Location $Location | Out-Null
-    Write-Host ($DmzRG + " is created") 
+    New-AzResourceGroup -Name $SpokeRG -Location $Location | Out-Null
+    Write-Host ("Resource Group " + $SpokeRG + " is created") 
 } else {
-    Write-Host ($DmzRG + " already exist") -ForegroundColor Yellow
+    Write-Host ("Resource Group " + $SpokeRG + " already exist") -ForegroundColor Yellow
 }
 Start-Sleep -Seconds 2
 #EndRegion Resource Group
@@ -33,58 +39,60 @@ Write-Host ("`n[LOG] " + (Get-Date -Format "yyyy-MM-dd hh:mm")) -ForegroundColor
 
 # Hub Virtual Network
 Write-Host "`Prepare Subnet: GatewaySubnet ..." -ForegroundColor Cyan
-$GatewaySubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix "10.82.251.0/26"
+$GatewaySubnet = New-AzVirtualNetworkSubnetConfig -Name GatewaySubnet -AddressPrefix "10.80.251.0/24"
 Start-Sleep -Milliseconds 200
 Write-Host "`nProvision Hub Virtual Network ..." -ForegroundColor Cyan
-$HubVirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $HubRG -Name "vnet-gpsn01-pd-eas-hub-01" -Location $Location -AddressPrefix "10.81.0.0/22","10.82.251.0/26" -Subnet $GatewaySubnet
+$HubVirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $HubRG -Name $HubVNetName -Location $Location -AddressPrefix "10.81.0.0/22","10.80.251.0/24" -Subnet $GatewaySubnet
 
-<#
-Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $HubVirtualNetwork -Name "Temp" -AddressPrefix "10.81.0.0/24" | Out-Null
-Write-Host ("`nAdd Subnet: " + "Temp") -ForegroundColor Cyan
-Start-Sleep -Milliseconds 200
-$HubVirtualNetwork | Set-AzVirtualNetwork | Out-Null
-#>
-
-# DMZ Virtual Network
+# Spoke Virtual Network
 Write-Host "`nPrepare Subnet: ApplicationGatewaySubnet ..." -ForegroundColor Cyan
-$DmzSubnet1 = New-AzVirtualNetworkSubnetConfig -Name ApplicationGatewaySubnet -AddressPrefix "10.81.20.64/26"
+$SpokeSubnet1 = New-AzVirtualNetworkSubnetConfig -Name ApplicationGatewaySubnet -AddressPrefix "10.81.20.64/26"
 Start-Sleep -Milliseconds 200
-Write-Host "`nProvision DMZ Virtual Network ..." -ForegroundColor Cyan
-$DmzVirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $DmzRG -Name "vnet-gpsn01-pd-eas-dmz-01" -Location $Location -AddressPrefix "10.81.20.0/22" -Subnet $DmzSubnet1
+Write-Host "`nProvision Spoke Virtual Network ..." -ForegroundColor Cyan
+$SpokeVirtualNetwork = New-AzVirtualNetwork -ResourceGroupName $SpokeRG -Name $SpokeVNetName -Location $Location -AddressPrefix "10.81.20.0/22" -Subnet $SpokeSubnet1
 #EndRegion Virtual Network
 
 #Region Public IP Address
 Write-Host ("`n[LOG] " + (Get-Date -Format "yyyy-MM-dd hh:mm")) -ForegroundColor White -BackgroundColor Black
-Write-Host "`nProvision Public IP Address for ExpressRoute Virtual Network Gateway ..." -ForegroundColor Cyan
-$pip = New-AzPublicIpAddress -ResourceGroupName $HubRG -Name "pip-ergw-gpsn01-pd-eas-hub-01" -AllocationMethod Static -Location $Location -Sku Standard -Zone (1,2,3)
+Write-Host "`nProvision Public IP Address for Virtual Network Gateway ..." -ForegroundColor Cyan
+$pip = New-AzPublicIpAddress -ResourceGroupName $HubRG -Name $pipName -AllocationMethod Static -Location $Location -Sku Standard -Zone (1,2,3)
 Start-Sleep -Seconds 30
 #EndRegion Public IP Address
 
-#Region ExpressRoute Virtual Network Gateway
+#Region Virtual Network Gateway
 Write-Host ("`n[LOG] " + (Get-Date -Format "yyyy-MM-dd hh:mm")) -ForegroundColor White -BackgroundColor Black
-Write-Host "`nProvision ExpressRoute Virtual Network Gateway ..." -ForegroundColor Cyan
-$VngIpConfig = New-AzVirtualNetworkGatewayIpConfig -Name "ergw-gpsn01-pd-eas-hub-01" -SubnetId ($HubVirtualNetwork.Subnets | ? {$_.Name -eq "GatewaySubnet"}).Id -PublicIpAddressId $pip.Id
+$VngIpConfig = New-AzVirtualNetworkGatewayIpConfig -Name "VngIpConfig" -SubnetId ($HubVirtualNetwork.Subnets | ? {$_.Name -eq "GatewaySubnet"}).Id -PublicIpAddressId $pip.Id
 Start-Sleep -Milliseconds 200
-$Vng = New-AzVirtualNetworkGateway -ResourceGroupName $HubRG -Name "ergw-gpsn01-pd-eas-hub-01" -Location $Location -IpConfigurations $VngIpConfig -GatewayType ExpressRoute -GatewaySku ErGw1AZ
-Start-Sleep -Seconds 30
-#EndRegion ExpressRoute Virtual Network Gateway
+
+# Require to provision in same resource group where Virtual Network with Gateway Subnet exist
+# Public IP Address of VNG can be located in different resource group
+if ($GatewayType -eq "ExpressRoute") {
+    Write-Host "`nProvision ExpressRoute Virtual Network Gateway ..." -ForegroundColor Cyan
+    $Vng = New-AzVirtualNetworkGateway -ResourceGroupName $HubRG -Name $VngName -Location $Location -IpConfigurations $VngIpConfig -GatewayType ExpressRoute -GatewaySku ErGw1AZ
+    Start-Sleep -Seconds 30
+} else {
+    Write-Host "`nProvision VPN Virtual Network Gateway ..." -ForegroundColor Cyan
+    $Vng = New-AzVirtualNetworkGateway -ResourceGroupName $HubRG -Name $VngName -Location $Location -IpConfigurations $VngIpConfig -GatewayType Vpn -GatewaySku VpnGw1AZ -VpnType "RouteBased"
+    Start-Sleep -Seconds 30
+}
+#EndRegion Virtual Network Gateway
 
 #Region Virtual Network Peering
 Write-Host ("`n[LOG] " + (Get-Date -Format "yyyy-MM-dd hh:mm")) -ForegroundColor White -BackgroundColor Black
 
 # Get Virtual Network Instance
 $HubVNet = Get-AzVirtualNetwork -ResourceGroup $HubRG -Name $HubVirtualNetwork.Name
-$DmzVNet = Get-AzVirtualNetwork -ResourceGroup $DmzRG -Name $DmzVirtualNetwork.Name
+$SpokeVNet = Get-AzVirtualNetwork -ResourceGroup $SpokeRG -Name $SpokeVirtualNetwork.Name
 
 # Setup Virtual Network Peering
-Write-Host ("`nAdd Peering between " + $HubVNet.Name + " and " + $DmzVNet.Name) -ForegroundColor Cyan
-Add-AzVirtualNetworkPeering -Name ("Peered-to-" + $DmzVNet.Name) -VirtualNetwork $HubVNet -RemoteVirtualNetworkId $DmzVNet.Id -AllowGatewayTransit -AllowForwardedTraffic | Out-Null
+Write-Host ("`nAdd Peering between " + $HubVNet.Name + " and " + $SpokeVNet.Name) -ForegroundColor Cyan
+Add-AzVirtualNetworkPeering -Name ("Peered-to-" + $SpokeVNet.Name) -VirtualNetwork $HubVNet -RemoteVirtualNetworkId $SpokeVNet.Id -AllowGatewayTransit -AllowForwardedTraffic | Out-Null
 Start-Sleep -Milliseconds 200
-Add-AzVirtualNetworkPeering -Name ("Peered-to-" + $HubVNet.Name) -VirtualNetwork $DmzVNet -RemoteVirtualNetworkId $HubVNet.Id -UseRemoteGateways -AllowForwardedTraffic | Out-Null
+Add-AzVirtualNetworkPeering -Name ("Peered-to-" + $HubVNet.Name) -VirtualNetwork $SpokeVNet -RemoteVirtualNetworkId $HubVNet.Id -UseRemoteGateways -AllowForwardedTraffic | Out-Null
 Start-Sleep -Seconds 15
 
 # Verification
-$Result = Get-AzVirtualNetworkPeering -ResourceGroupName $HubRG -VirtualNetworkName $HubVNet.Name -Name ("Peered-to-" + $DmzVNet.Name) 
+$Result = Get-AzVirtualNetworkPeering -ResourceGroupName $HubRG -VirtualNetworkName $HubVNet.Name -Name ("Peered-to-" + $SpokeVNet.Name) 
 if ($Result.PeeringState -ne "Connected") {
     Write-Host "Peering state is failed" -ForegroundColor Yellow
 } 
@@ -99,3 +107,11 @@ $Duration = $EndTime - $StartTime
 Write-Host ("`nTotal Process Time: " + $Duration.Minutes + " Minutes " + $Duration.Seconds + " Seconds") -ForegroundColor White -BackgroundColor Black
 Start-Sleep -Seconds 1
 Write-Host "`n`n"
+
+#Region Decommission
+Remove-AzVirtualNetworkGateway -ResourceGroupName $HubRG -Name $VngName -Force -Confirm:$false
+Start-Sleep -Seconds 10
+Remove-AzPublicIpAddress -ResourceGroupName $HubRG -Name $pipName -Force -Confirm:$false
+Remove-AzResourceGroup -Name $HubRG -Force -Confirm:$false
+Remove-AzResourceGroup -Name $SpokeRG -Force -Confirm:$false
+#EndRegion Decommission
